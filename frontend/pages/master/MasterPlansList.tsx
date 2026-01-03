c632d3f5f9e6956d3cf6afc1695f10b3763f5c2e
Antônio Ferreira <rabf44@gmail.com>
2026-01-03 11:38:48 -0300

    fix: import useNavigate in MasterPlansList

--- frontend/pages/master/MasterPlansList.tsx
+++ frontend/pages/master/MasterPlansList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { getPlans } from '../../services/masterApi';
 import MasterLayout from '../../components/layouts/MasterLayout';
 import { getApiErrorMessage, logErrorInDev } from '../../utils/getApiErrorMessage';
